63f4326cbd47d8926b9e484e8b35e3e392ab53c1
unknown <Eduarda Nunes>
2022-10-19 22:06:44 -0300

    exercicio2

---
+++ script.js
@@ -0,0 +1,6 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 7;
+
+var mediaFinal = (nota1 * 0.2 + nota2 * 0.3 + nota3 * 0.5);
+console.log(mediaFinal);                                                     
